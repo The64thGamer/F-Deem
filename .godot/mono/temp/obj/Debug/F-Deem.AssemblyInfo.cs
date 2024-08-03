d72b6a1f6f0cd18057cdbbbf033b13514b87f943
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-08-03 13:35:48 -0500

    stuff

--- .godot/mono/temp/obj/Debug/F-Deem.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/F-Deem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F-Deem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5e4c96b8590df4ab3533f757558eaf15c4dc84")]
 [assembly: System.Reflection.AssemblyProductAttribute("F-Deem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F-Deem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
